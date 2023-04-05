66759ba92f16d42dcc355b2e857fc770d63fd396
minsun <elinnane0@gmail.com>
2023-04-05 11:05:12 +0900

    update & login, sign Activity

---
+++ safe/app/src/main/java/com/abb/safe/RegisterActivity.java
@@ -0,0 +1,114 @@
+package com.abb.safe;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    FirebaseFirestore db;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextName;
+    private EditText editTextBirth;
+    private Button buttonJoin;
+    private Button buttonJoin_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        firebaseAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        editTextEmail = (EditText) findViewById(R.id.editText_email);
+        editTextPassword = (EditText) findViewById(R.id.editText_passWord);
+        editTextName = (EditText) findViewById(R.id.editText_name);
+        editTextBirth = (EditText) findViewById(R.id.editText_birth);
+
+        buttonJoin = (Button) findViewById(R.id.btn_join);
+        buttonJoin_login = (Button) findViewById(R.id.btn_join_login);
+        buttonJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!editTextEmail.getText().toString().equals("") && !editTextPassword.getText().toString().equals("")) {
+                    // 이메일과 비밀번호가 공백이 아닌 경우
+                    createUser(editTextEmail.getText().toString(), editTextPassword.getText().toString(), editTextName.getText().toString(), editTextBirth.getText().toString());
+                } else {
+                    // 이메일과 비밀번호가 공백인 경우
+                    Toast.makeText(RegisterActivity.this, "계정과 비밀번호를 입력하세요.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        buttonJoin_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void createUser(String email, String password, String name, String birth) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 회원가입 성공시 db에 데이터 저장
+                            Map<String, Object> data = new HashMap<>();
+                            data.put("id", email);
+                            data.put("name", name);
+                            data.put("birth", birth);
+
+                            db.collection("members")
+                                    .add(data)
+                                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                        @Override
+                                        public void onSuccess(DocumentReference documentReference) {
+                                            Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w(TAG, "Error adding document", e);
+                                        }
+                                    });
+
+                            Log.d("sign up", "onComplete: "+task.getResult());
+                            Toast.makeText(RegisterActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // 계정이 중복된 경우, 이메일 형식이아니거나, 비밀번호가 6자리 이상이 아닐 때 발생
+                            Toast.makeText(RegisterActivity.this, "이미 존재하는 계정입니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
--- safe/app/src/main/java/com/abb/safe/SettingActivity.java
+++ safe/app/src/main/java/com/abb/safe/SettingActivity.java
@@ -3,10 +3,14 @@ package com.abb.safe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -45,5 +49,14 @@ public class SettingActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String email = user.getEmail();
+            Log.d("email", "onCreate: " + email);
+
+
+        }
     }
+
 }
