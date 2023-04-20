719771ed2162bfef18fd20010462990782fe1ac5
minsun <elinnane0@gmail.com>
2023-04-20 21:37:31 +0900

    update ignore
    Former-commit-id: c33b6d9522b5f71e0c34ae35d87e0571cc26e390

--- safe/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,4 +0,0 @@
-#include "il2cpp-config.h"
-#include "/Users/msun/Documents/ms/safe/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/codegen/il2cpp-codegen-common.cpp"
-#include "/Users/msun/Documents/ms/safe/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/codegen/il2cpp-codegen-il2cpp.cpp"
-#include "/Users/msun/Documents/ms/safe/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/codegen/il2cpp-codegen-tiny.cpp"
--- safe/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_vm-utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "il2cpp-config.h"
-#include "/Users/msun/Documents/ms/safe/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/vm-utils/BlobReader.cpp"
-#include "/Users/msun/Documents/ms/safe/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/vm-utils/Debugger.cpp"
-#include "/Users/msun/Documents/ms/safe/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/vm-utils/NativeDelegateMethodCache.cpp"
-#include "/Users/msun/Documents/ms/safe/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/vm-utils/NativeSymbol.cpp"
-#include "/Users/msun/Documents/ms/safe/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/vm-utils/VmStringUtils.cpp"
-#include "/Users/msun/Documents/ms/safe/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/vm-utils/icalls/mscorlib/System.Threading/Interlocked.cpp"
-#include "/Users/msun/Documents/ms/safe/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/vm-utils/icalls/mscorlib/System/Math.cpp"
